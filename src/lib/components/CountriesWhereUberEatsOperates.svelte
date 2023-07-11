ad85c967264ab2db5e432e2a7db8ec375ebdbfd2
Seba9989 <sgrzybbowski@gmail.com>
2023-07-11 18:05:25 +0200

    Add CountriesWhereUberEatsOperates

---
+++ src/lib/components/CountriesWhereUberEatsOperates.svelte
@@ -0,0 +1,157 @@
+<script lang="ts">
+	interface CountryType {
+		text: string;
+		url: string;
+	}
+
+	let CountriesList: CountryType[] = [
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		},
+		{
+			text: 'Australia',
+			url: 'Belgia'
+		}
+	];
+</script>
+
+<h2>Kraje, w których działa Uber Eats</h2>
+<div class="grid">
+	{#each CountriesList as Country}
+		<a href={Country.url}>
+			{Country.text}
+		</a>
+	{/each}
+</div>
+
+<style lang="scss">
+	h2 {
+		margin-top: 80px;
+
+		font-size: 36px;
+		font-weight: 700;
+	}
+
+	.grid {
+		margin-top: 24px;
+
+		display: grid;
+		grid-template-columns: auto auto auto auto;
+		grid-row: auto auto auto auto auto auto auto auto;
+
+		gap: 24px;
+
+		* {
+			font-weight: normal;
+		}
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,6 @@
 <script>
 	import CitiesNearby from '$lib/components/CitiesNearby.svelte';
+	import CountriesWhereUberEatsOperates from '$lib/components/CountriesWhereUberEatsOperates.svelte';
 	import FirstFullSide from '$lib/components/FirstFullSide.svelte';
 	import Imges from '$lib/components/Imges.svelte';
 </script>
@@ -9,10 +10,33 @@
 <div class="padding">
 	<Imges />
 	<CitiesNearby />
+	<CountriesWhereUberEatsOperates />
+	<div class="link">
+		<a href="/">
+			Dowiedz się, jak Uber Eats ustala kolejność lokali w kanale. <div>Więcej informacji</div>
+		</a>
+	</div>
 </div>
 
 <style lang="scss">
 	.padding {
 		padding: 0 40px;
+	}
+
+	.link {
+		margin-top: 24px;
+		display: flex;
+		justify-content: center;
+
+		a {
+			font-weight: 400;
+			display: flex;
+			width: fit-content;
+		}
+
+		* {
+			font-weight: 400;
+			text-decoration: underline;
+		}
 	}
 </style>
