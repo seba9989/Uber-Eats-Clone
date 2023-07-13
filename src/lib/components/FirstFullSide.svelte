39870625e00aae364bc953e8e83ecfc701181c5c
Seba9989 <sgrzybbowski@gmail.com>
2023-07-13 12:56:39 +0200

    Add First Full Side Mobile

--- src/lib/components/FirstFullSide.svelte
+++ src/lib/components/FirstFullSide.svelte
@@ -19,36 +19,65 @@
 	let deliveryDate: DeliveryDate;
 
 	let listOpen = false;
+
+	let innerWidth: number;
+
+	let mobileVersion: boolean;
+
+	$: if (innerWidth != undefined) {
+		mobileVersion = innerWidth < 500;
+	}
 </script>
+
+<svelte:window bind:innerWidth />
 
 <main>
 	<div>
-		<h1>Zamów jedzenie pod drzwi</h1>
+		{#if !mobileVersion}
+			<h1>Zamów jedzenie pod drzwi</h1>
+		{:else}
+			<h2>Zamów jedzenie pod drzwi</h2>
+		{/if}
 		<div class="search-bar">
 			<div class="text-input">
-				<span class="material-symbols-outlined"> location_on </span>
+				<svg
+					width="24px"
+					height="24px"
+					fill="none"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true"
+					focusable="false"
+				>
+					<path
+						d="M17.5834 5.16602C14.5001 2.08268 9.50008 2.08268 6.41675 5.16602C3.33341 8.24935 3.33341 13.3327 6.41675 16.416L12.0001 21.9993L17.5834 16.3327C20.6667 13.3327 20.6667 8.24935 17.5834 5.16602ZM12.0001 12.416C11.0834 12.416 10.3334 11.666 10.3334 10.7493C10.3334 9.83268 11.0834 9.08268 12.0001 9.08268C12.9167 9.08268 13.6667 9.83268 13.6667 10.7493C13.6667 11.666 12.9167 12.416 12.0001 12.416Z"
+						fill="currentColor"
+					/>
+				</svg>
 				<input type="text" placeholder="Wpisz adres dostawy" />
 			</div>
 
 			<button class="select-menu" on:click={() => (listOpen = !listOpen)}>
 				{#if deliveryType === 'now'}
-					<svg
-						width="20px"
-						height="20px"
-						fill="none"
-						viewBox="0 0 24 24"
-						xmlns="http://www.w3.org/2000/svg"
-						aria-hidden="true"
-						focusable="false"
-					>
-						<path
-							d="M12 2.83398C6.91671 2.83398 2.83337 6.91732 2.83337 12.0007C2.83337 17.084 6.91671 21.1673 12 21.1673C17.0834 21.1673 21.1667 17.084 21.1667 12.0007C21.1667 6.91732 17.0834 2.83398 12 2.83398ZM17 13.6673H10.3334V5.33398H12.8334V11.1673H17V13.6673Z"
-							fill="currentColor"
-						/>
-					</svg>
+					<div class="icon">
+						<svg
+							width="20px"
+							height="20px"
+							fill="none"
+							viewBox="0 0 24 24"
+							xmlns="http://www.w3.org/2000/svg"
+							aria-hidden="true"
+							focusable="false"
+						>
+							<path
+								d="M12 2.83398C6.91671 2.83398 2.83337 6.91732 2.83337 12.0007C2.83337 17.084 6.91671 21.1673 12 21.1673C17.0834 21.1673 21.1667 17.084 21.1667 12.0007C21.1667 6.91732 17.0834 2.83398 12 2.83398ZM17 13.6673H10.3334V5.33398H12.8334V11.1673H17V13.6673Z"
+								fill="currentColor"
+							/>
+						</svg>
+					</div>
 					<p>Dostarcz teraz</p>
 				{:else if deliveryDate}
-					<p>
+					<div class="icon">
 						<svg
 							width="20px"
 							height="20px"
@@ -65,12 +94,30 @@
 								fill="currentColor"
 							/>
 						</svg>
+					</div>
+					<p>
 						{deliveryDate.data.day}-{deliveryDate.data.month}-{deliveryDate.data.year},
 						{deliveryDate.time.hour}:{deliveryDate.time.minutes}
 					</p>
 				{/if}
 
-				<span class="material-symbols-outlined icon"> expand_more </span>
+				<svg
+					width="24px"
+					height="24px"
+					fill="none"
+					viewBox="0 0 24 24"
+					xmlns="http://www.w3.org/2000/svg"
+					aria-hidden="true"
+					focusable="false"
+					style="position: absolute; right: 4px; padding: 12px;"
+					class="icon"
+				>
+					<path
+						d="M17 11.7494V14.916L12 11.0827L7 14.916V11.7494L12 7.91602L17 11.7494Z"
+						fill="#000000"
+						transform="rotate(180, 12, 12)"
+					/>
+				</svg>
 
 				{#if listOpen}
 					<div class="absolute">
@@ -153,6 +200,16 @@
 
 		margin-bottom: 80px;
 
+		@media (max-width: 500px) {
+			justify-content: start;
+
+			background-image: url('https://d3i4yxtzktqr9n.cloudfront.net/web-eats-v2/3dec1d1b7e1b5874.png');
+
+			padding: 0 16px;
+
+			padding-top: 72px;
+		}
+
 		h1 {
 			margin-bottom: 40px;
 
@@ -161,9 +218,18 @@
 			line-height: 64px;
 		}
 
+		h2 {
+			font-size: 36px;
+			font-weight: 700;
+		}
+
 		.search-bar {
 			display: flex;
 			gap: 8px;
+
+			@media (max-width: 500px) {
+				flex-direction: column;
+			}
 
 			* {
 				font-size: 1rem;
@@ -172,6 +238,10 @@
 			.text-input {
 				width: 540px;
 				height: 56px;
+
+				@media (max-width: 500px) {
+					width: 100%;
+				}
 
 				display: flex;
 				align-items: center;
@@ -219,8 +289,9 @@
 				}
 
 				.icon {
-					aspect-ratio: 1/1;
+					/* aspect-ratio: 1/1; */
 					height: 48px;
+					width: 48px;
 
 					display: flex;
 					justify-content: center;
@@ -302,6 +373,8 @@
 				text-decoration: underline;
 			}
 			margin-top: 24px;
+
+			font-size: 14px;
 		}
 	}
 </style>
