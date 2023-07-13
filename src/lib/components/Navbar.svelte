855fd7be1abb0ee7cc87dca60668356179a170d8
Seba9989 <sgrzybbowski@gmail.com>
2023-07-13 12:11:10 +0200

    Add Navbar mobile version

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,7 +2,20 @@
 	import { hamburgerOpen } from '$lib/ts/Stores';
 
 	let y: number;
+
+	let innerWidth: number;
+
+	let mobileVersion: boolean;
+
+	$: if (innerWidth != undefined) {
+		mobileVersion = innerWidth > 500;
+		// console.log(mobileVersion);
+	}
+
+	// $: console.log(innerWidth);
 </script>
+
+<svelte:window bind:scrollY={y} bind:innerWidth />
 
 <div class="h0">
 	<header class:white={y > 0}>
@@ -16,42 +29,44 @@
 			</button>
 			<img src="/UberLogo.svg" alt="Uber Eats" />
 
-			<div class="text-input" class:show={y > 450}>
-				<svg
-					width="24px"
-					height="24px"
-					fill="none"
-					viewBox="0 0 24 24"
-					xmlns="http://www.w3.org/2000/svg"
-					aria-hidden="true"
-					focusable="false"
-				>
-					<path
-						d="M17.5834 5.16602C14.5001 2.08268 9.50008 2.08268 6.41675 5.16602C3.33341 8.24935 3.33341 13.3327 6.41675 16.416L12.0001 21.9993L17.5834 16.3327C20.6667 13.3327 20.6667 8.24935 17.5834 5.16602ZM12.0001 12.416C11.0834 12.416 10.3334 11.666 10.3334 10.7493C10.3334 9.83268 11.0834 9.08268 12.0001 9.08268C12.9167 9.08268 13.6667 9.83268 13.6667 10.7493C13.6667 11.666 12.9167 12.416 12.0001 12.416Z"
-						fill="currentColor"
-					/>
-				</svg>
-				<input type="text" placeholder="Wpisz adres dostawy" />
-			</div>
+			{#if mobileVersion}
+				<div class="text-input" class:show={y > 450}>
+					<svg
+						width="24px"
+						height="24px"
+						fill="none"
+						viewBox="0 0 24 24"
+						xmlns="http://www.w3.org/2000/svg"
+						aria-hidden="true"
+						focusable="false"
+					>
+						<path
+							d="M17.5834 5.16602C14.5001 2.08268 9.50008 2.08268 6.41675 5.16602C3.33341 8.24935 3.33341 13.3327 6.41675 16.416L12.0001 21.9993L17.5834 16.3327C20.6667 13.3327 20.6667 8.24935 17.5834 5.16602ZM12.0001 12.416C11.0834 12.416 10.3334 11.666 10.3334 10.7493C10.3334 9.83268 11.0834 9.08268 12.0001 9.08268C12.9167 9.08268 13.6667 9.83268 13.6667 10.7493C13.6667 11.666 12.9167 12.416 12.0001 12.416Z"
+							fill="currentColor"
+						/>
+					</svg>
+					<input type="text" placeholder="Wpisz adres dostawy" />
+				</div>
+			{/if}
 		</div>
 
 		<nav class="right-side">
 			<a href="/logIn" class="login">
-				<svg aria-hidden="true" focusable="false" viewBox="0 0 26 26" class="cm cn co cp">
+				<svg aria-hidden="true" focusable="false" viewBox="0 0 26 26">
 					<path
 						fill-rule="evenodd"
 						clip-rule="evenodd"
 						d="M18.958 7.042a5.958 5.958 0 11-11.916 0 5.958 5.958 0 0111.916 0zM3.25 21.667c0-3.575 2.925-6.5 6.5-6.5h6.5c3.575 0 6.5 2.925 6.5 6.5v3.25H3.25v-3.25z"
 					/>
 				</svg>
-				Zaloguj się
+				{#if mobileVersion}
+					Zaloguj się
+				{/if}
 			</a>
 			<a href="/singIn" class="singin"> Zarejestruj się</a>
 		</nav>
 	</header>
 </div>
-
-<svelte:window bind:scrollY={y} />
 
 <style lang="scss">
 	.h0 {
@@ -69,6 +84,11 @@
 		justify-content: space-between;
 		align-items: center;
 
+		@media (max-width: 500px) {
+			& {
+				padding: 0 16px;
+			}
+		}
 		padding: 0 40px;
 
 		box-shadow: none;
@@ -87,6 +107,12 @@
 			align-items: center;
 			gap: 32px;
 
+			@media (max-width: 500px) {
+				&{
+					gap: 24px;
+				}
+			}
+
 			.hamburger-menu {
 				aspect-ratio: 1 / 1;
 				height: 24px;
@@ -96,6 +122,12 @@
 
 				.svg {
 					width: 20px;
+				}
+			}
+
+			@media (max-width: 500px) {
+				img {
+					height: 20px;
 				}
 			}
 		}
@@ -154,18 +186,26 @@
 			font-weight: 500;
 
 			a {
-				min-height: 48px;
+				display: flex;
+				align-items: center;
 
 				padding: 12px 16px;
 
 				border-radius: 500px;
 
 				box-shadow: rgba(0, 0, 0, 0.1) 0px 0px 8px, rgba(0, 0, 0, 0.04) 0px 4px 4px;
+
+				@media (max-width: 500px) {
+					& {
+						padding: 12px;
+						white-space: nowrap;
+						font-size: 14px;
+						height: 36px;
+					}
+				}
 			}
 
 			.login {
-				display: flex;
-
 				gap: 4px;
 
 				background: #fff;
@@ -175,7 +215,13 @@
 				}
 
 				svg {
+					height: 20px;
 					width: 20px;
+
+					@media (max-width: 500px) {
+						height: 16px;
+						width: 16px;
+					}
 				}
 			}
 
